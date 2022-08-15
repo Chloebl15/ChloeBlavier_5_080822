c18cbcbef3331662c56e2f1a270131b09b12b86d
Chloé <chloeblavier@hotmail.com>
2022-08-15 17:59:23 +0100

    Ajout d'un boucle

---
+++ front/js/index.js
@@ -0,0 +1,33 @@
+let articles = [];
+//Création tableau 
+
+const getArticles = async () => {
+    await fetch("http://localhost:3000/api/products")
+        .then((res) => res.json())
+        .then((data) => {
+            articles = data
+            console.log(articles)
+            //Ajout de la promesse dans le tableau articles
+        })
+        .catch((error) => {
+            // Si erreur dans URL, retourner l'erreur
+            console.log(error);
+            //afficher l'erreur 
+        })
+};
+
+const creationArticles = async () => {
+    await getArticles();
+    for (let article of articles) {
+        document.getElementById("items").innerHTML += ` <a href="./product.html?id=${article._id}">
+    <article>
+      <img src="${articles.imageUrl}" alt="${articles.altTxt}">
+      <h3 class="productName">${articles.name}</h3>
+      <p class="productDescription">${articles.description}</p>
+    </article>
+  </a> `
+    }
+}
+
+
+creationArticles();
